29745c368c3c88b59701fcf347ab0fbfcb7a2e3b
saul <dlathf3210@naver.com>
2023-05-16 09:43:44 +0900

    first commit

---
+++ src/main/java/com/example/recommendpharmacy/RecommendPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.example.recommendpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendPharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendPharmacyApplication.class, args);
+    }
+
+}
