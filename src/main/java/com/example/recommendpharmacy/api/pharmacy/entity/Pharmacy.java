34093af05e56773cd298a45f97448ca2ec6dc784
saul <dlathf3210@naver.com>
2023-05-18 11:32:41 +0900

    Pharmcy Entity create

---
+++ src/main/java/com/example/recommendpharmacy/api/pharmacy/entity/Pharmacy.java
@@ -0,0 +1,32 @@
+package com.example.recommendpharmacy.api.pharmacy.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "pharmacy")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pharmacy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String pharmacyName;
+
+    private String pharmacyAddress;
+
+    private double latitude; //위도, Y
+
+    private double longitude; // 경도, X
+
+}
---
+++ src/main/java/com/example/recommendpharmacy/api/pharmacy/repository/PharmacyRepository.java
@@ -0,0 +1,9 @@
+package com.example.recommendpharmacy.api.pharmacy.repository;
+
+import com.example.recommendpharmacy.api.pharmacy.entity.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacyRepository extends JpaRepository<Long, Pharmacy> {
+
+
+}
