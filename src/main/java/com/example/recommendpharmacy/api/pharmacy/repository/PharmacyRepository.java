f01e40033057a9b74aa815025c4f0c5491bbc8c0
saul <dlathf3210@naver.com>
2023-05-18 13:21:38 +0900

    PharmacyRepository update

--- src/main/java/com/example/recommendpharmacy/api/pharmacy/repository/PharmacyRepository.java
+++ src/main/java/com/example/recommendpharmacy/api/pharmacy/repository/PharmacyRepository.java
@@ -3,7 +3,7 @@ package com.example.recommendpharmacy.api.pharmacy.repository;
 import com.example.recommendpharmacy.api.pharmacy.entity.Pharmacy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PharmacyRepository extends JpaRepository<Long, Pharmacy> {
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
 
 
 }
