727ae3b2e852861338cb29178dc84e44215cf4ad
saul <dlathf3210@naver.com>
2023-05-20 20:12:09 +0900

    KakaoAddressSearchServiceTest update

--- src/test/groovy/com/example/recommendpharmacy/api/service/KakaoAddressSearchServiceTest.groovy
+++ src/test/groovy/com/example/recommendpharmacy/api/service/KakaoAddressSearchServiceTest.groovy
@@ -1,9 +1,11 @@
 package com.example.recommendpharmacy.api.service
 
 import com.example.recommendpharmacy.AbstractIntegrationContainerBaseTest
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import spock.lang.Specification
 
+@Slf4j
 class KakaoAddressSearchServiceTest extends AbstractIntegrationContainerBaseTest {
 
     @Autowired
@@ -23,6 +25,17 @@ class KakaoAddressSearchServiceTest extends AbstractIntegrationContainerBaseTest
 
 
     def "주소값이 valid하다면, requestAddressSearch 메소드는 정상적으로 documnet를 반환한다"(){
+        given:
+        def address = "서울 성북구 종암로 10길"
+
+        when:
+        def result = addressSearchService.requestAddressSearch(address)
+
+        then:
+        result.getDocumentList().size() > 0
+        result.metaDto.totalCount > 0
+        result.documentList.get(0).addressName != null
+
 
     }
 
